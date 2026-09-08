63e6a6d305a684f2aaea282b2e47bbe1147ee32e
domi41 <dominique.merle.41@gmail.com>
2026-09-08 14:01:03 +0200

    config: use the relative minimum in the proximity spider plot
    Also, add a small outline to the tick values, to make them more readable.

--- app/src/main/java/com/illiouchine/jm/ui/composable/plot/lib/SpiderChart.kt
+++ app/src/main/java/com/illiouchine/jm/ui/composable/plot/lib/SpiderChart.kt
@@ -13,8 +13,12 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -75,9 +79,23 @@ fun SpiderChart(
             radialAxisModel = rememberFloatRadialAxisModel(tickValues),
             angularAxisModel = rememberCategoryAngularAxisModel(indexedCategories),
             radialAxisLabels = {
+                val labelValue = it.toDouble().smartFormat(tickDecimals)
+                val labelStyle = TextStyle.Default
+                // Outline
                 Text(
-                    text = it.toDouble().smartFormat(tickDecimals),
-                    style = TextStyle.Default,
+                    text = labelValue,
+                    style = labelStyle.copy(
+                        color = Theme.colorScheme.background,
+                        drawStyle = Stroke(
+                            width = 6f,
+                            join = StrokeJoin.Round,
+                        ),
+                    ),
+                )
+                // Fill
+                Text(
+                    text = labelValue,
+                    style = labelStyle,
                 )
             },
             angularAxisLabels = {
