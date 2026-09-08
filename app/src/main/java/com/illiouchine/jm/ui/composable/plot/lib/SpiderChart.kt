93d3aebe3fa6c623066d748c1cccf9bee1dea445
domi41 <dominique.merle.41@gmail.com>
2026-09-08 14:09:46 +0200

    chore(lint): unused imports

--- app/src/main/java/com/illiouchine/jm/ui/composable/plot/lib/SpiderChart.kt
+++ app/src/main/java/com/illiouchine/jm/ui/composable/plot/lib/SpiderChart.kt
@@ -13,12 +13,10 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
